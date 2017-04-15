74000a7c9bf18c717eb80959a994cdc0d4dea1ba
snowair <xcoolcc@gmail.com>
2017-04-15 18:07:21 +0800

    update 1.3.x

--- 1.3.4/Phalcon/Db/Index.php
+++ 1.3.4/Phalcon/Db/Index.php
@@ -2,63 +2,60 @@
 
 namespace Phalcon\Db {
 
-	/**
-	 * Phalcon\Db\Index
-	 *
-	 * Allows to define indexes to be used on tables. Indexes are a common way
-	 * to enhance database performance. An index allows the database server to find
-	 * and retrieve specific rows much faster than it could do without an index
-	 */
-	
-	class Index implements \Phalcon\Db\IndexInterface {
+    /**
+     * Phalcon\Db\Index
+     *
+     * Allows to define indexes to be used on tables. Indexes are a common way
+     * to enhance database performance. An index allows the database server to find
+     * and retrieve specific rows much faster than it could do without an index
+     */
+    class Index implements \Phalcon\Db\IndexInterface
+    {
 
-		protected $_indexName;
+        protected $_indexName;
 
-		protected $_columns;
+        protected $_columns;
 
-		protected $_type;
-
-		/**
-		 * \Phalcon\Db\Index constructor
-		 *
-		 * @param string $indexName
-		 * @param array $columns
-		 * @param string|null $type
-		 */
-		public function __construct($indexName, $columns, $type=null){ }
-
-
-		/**
-		 * Gets the index name
-		 *
-		 * @return string
-		 */
-		public function getName(){ }
+        /**
+         * \Phalcon\Db\Index constructor
+         *
+         * @param string $indexName
+         * @param array $columns
+         */
+        public function __construct($indexName, $columns)
+        {
+        }
 
 
-		/**
-		 * Gets the columns that comprends the index
-		 *
-		 * @return array
-		 */
-		public function getColumns(){ }
+        /**
+         * Gets the index name
+         *
+         * @return string
+         */
+        public function getName()
+        {
+        }
 
 
-		/**
-		 * Gets the index type
-		 *
-		 * @return string
-		 */
-		public function getType(){ }
+        /**
+         * Gets the columns that comprends the index
+         *
+         * @return array
+         */
+        public function getColumns()
+        {
+        }
 
 
-		/**
-		 * Restore a \Phalcon\Db\Index object from export
-		 *
-		 * @param array $data
-		 * @return \Phalcon\Db\IndexInterface
-		 */
-		public static function __set_state($properties=null){ }
+        /**
+         * Restore a \Phalcon\Db\Index object from export
+         *
+         * @param array $data
+         * @return \Phalcon\Db\IndexInterface
+         */
+        public static function __set_state($properties=null)
+        {
+        }
 
-	}
+    }
 }
--- 1.3.4/Phalcon/Queue/Beanstalk/Job.php
+++ 1.3.4/Phalcon/Queue/Beanstalk/Job.php
@@ -2,96 +2,111 @@
 
 namespace Phalcon\Queue\Beanstalk {
 
-	/**
-	 * Phalcon\Queue\Beanstalk\Job
-	 *
-	 * Represents a job in a beanstalk queue
-	 */
-	
-	class Job {
+    /**
+     * Phalcon\Queue\Beanstalk\Job
+     *
+     * Represents a job in a beanstalk queue
+     */
+    class Job
+    {
 
-		protected $_queue;
+        protected $_queue;
 
-		protected $_id;
+        protected $_id;
 
-		protected $_body;
+        protected $_body;
 
-		/**
-		 * \Phalcon\Queue\Beanstalk\Job
-		 *
-		 * @param \Phalcon\Queue\Beanstalk $queue
-		 * @param string $id
-		 * @param mixed $body
-		 */
-		public function __construct($queue, $id, $body){ }
-
-
-		/**
-		 * Returns the job id
-		 *
-		 * @return string
-		 */
-		public function getId(){ }
+        /**
+         * \Phalcon\Queue\Beanstalk\Job
+         *
+         * @param \Phalcon\Queue\Beanstalk $queue
+         * @param string $id
+         * @param mixed $body
+         */
+        public function __construct($queue, $id, $body)
+        {
+        }
 
 
-		/**
-		 * Returns the job body
-		 *
-		 * @return mixed
-		 */
-		public function getBody(){ }
+        /**
+         * Returns the job id
+         *
+         * @return string
+         */
+        public function getId()
+        {
+        }
 
 
-		/**
-		 * Removes a job from the server entirely
-		 *
-		 * @return boolean
-		 */
-		public function delete(){ }
+        /**
+         * Returns the job body
+         *
+         * @return mixed
+         */
+        public function getBody()
+        {
+        }
 
 
-		/**
-		 * The release command puts a reserved job back into the ready queue (and marks
-		 * its state as "ready") to be run by any client. It is normally used when the job
-		 * fails because of a transitory error.
-		 *
-		 * @return boolean
-		 */
-		public function release(){ }
+        /**
+         * Removes a job from the server entirely
+         *
+         * @return boolean
+         */
+        public function delete()
+        {
+        }
 
 
-		/**
-		 * The bury command puts a job into the "buried" state. Buried jobs are put into
-		 * a FIFO linked list and will not be touched by the server again until a client
-		 * kicks them with the "kick" command.
-		 *
-		 * @return boolean
-		 */
-		public function bury(){ }
+        /**
+         * The release command puts a reserved job back into the ready queue (and marks
+         * its state as "ready") to be run by any client. It is normally used when the job
+         * fails because of a transitory error.
+         *
+         * @return boolean
+         */
+        public function release()
+        {
+        }
 
 
-		/**
-		 * The `touch` command allows a worker to request more time to work on a job. 
-		 * This is useful for jobs that potentially take a long time, but you still
-		 * want the benefits of a TTR pulling a job away from an unresponsive worker.
-		 * A worker may periodically tell the server that it's still alive and processing
-		 * a job (e.g. it may do this on `DEADLINE_SOON`). The command postpones the auto
-		 * release of a reserved job until TTR seconds from when the command is issued.
-		 *
-		 * @return boolean
-		 */
-		public function touch(){ }
+        /**
+         * The bury command puts a job into the "buried" state. Buried jobs are put into
+         * a FIFO linked list and will not be touched by the server again until a client
+         * kicks them with the "kick" command.
+         *
+         * @return boolean
+         */
+        public function bury()
+        {
+        }
 
 
-		/**
-		 * Move the job to the ready queue if it is delayed or buried.
-		 *
-		 * @return boolean
-		 */
-		public function kick(){ }
+        /**
+         * The bury command puts a job into the "buried" state. Buried jobs are put into
+         * a FIFO linked list and will not be touched by the server again until a client
+         * kicks them with the "kick" command.
+         *
+         * @return boolean
+         */
+        public function touch()
+        {
+        }
 
 
-		public function __wakeup(){ }
+        /**
+         * Move the job to the ready queue if it is delayed or buried.
+         *
+         * @return boolean
+         */
+        public function kick()
+        {
+        }
 
-	}
+
+        public function __wakeup()
+        {
+        }
+
+    }
 }
